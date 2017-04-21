c61dc3798b9e26fd3be4b95736514c8682c639fa
Koushik Paul <paul@Koushiks-MacBook-Air.local>
2017-04-20 22:32:59 -0400

    hw5 added

---
+++ hw5/NYUCodebase/Entity.h
@@ -0,0 +1,55 @@
+#include "Matrix.h"
+#include <SDL_image.h>
+#include "ShaderProgram.h"
+#include "stb_image.h"
+#include <vector>
+
+struct Vector2
+{
+    float x;
+    float y;
+};
+
+class Entity
+{
+public:
+	Vector2 position;
+	Vector2 direction;
+	Vector2 length;
+    Vector2 acceleration;
+    Vector2 friction;
+    
+    float gravity;
+	float top;
+	float bottom;
+	float right;
+	float left;
+    
+	float textureTop;
+	float textureBottom;
+	float textureRight;
+	float textureLeft;
+    
+    float textureCoords[12];
+    GLuint texture;
+    static GLuint LoadTexture(const char *filePath);
+    void addTexture(std::string name);
+    
+    bool Debug;
+    bool Active;
+    
+    Matrix modelMatrix;
+
+    Entity(float Width=0.0f, float Height=0.0f, float x=0.0f, float y=0.0f);
+    void updateAll();
+    void updateLength(Vector2 newLength);
+    
+    void Draw(ShaderProgram &program);
+    void Move(float ticks);
+    void CalcTextureCoords(int totalWidth, int totalHeight, int Width, int Height, int Left, int Top, int LRoffset=0, int UDoffset=0);
+    void changeSize(float changeByThisMuch);
+
+    static std::vector<Entity> printText(Vector2 postion, std::string line, float changeSize);
+    float lerp(float v0, float v1, float t);
+    bool collisionDetect(Entity object);
+};
